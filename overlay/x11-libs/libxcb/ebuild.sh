dd3c5122957f82cf4ae2a8e537157b70bdd0f1ab
James Le Cuirot <chewi@aura-online.co.uk>
2014-07-29 20:47:57 +0100

    Fixed in the tree.

--- overlay/x11-libs/libxcb/ebuild.sh
+++
@@ -1,9 +0,0 @@
-# Gentoo bug #472224.
-
-EAPI=5
-source "${PORTDIR}/${CATEGORY}/${PN}/${BASH_SOURCE[0]##*/}"
-EAPI=5
-
-python_check_deps() {
-	has_version --host-root $(echo "${DEPEND}" | grep -o "[^ ]*x11-proto/xcb-proto[^ ]*" | head -n1)
-}
