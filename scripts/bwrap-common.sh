b417db218bb7cbc17d4ea4e09985e07bbe37a150
James Le Cuirot <chewi@gentoo.org>
2024-05-20 15:32:31 +0100

    Check that QEMU interpreters exist before bind mounting them
    systemd registers all the interpreters, even the ones you don't have
    installed.

--- scripts/bwrap-common.sh
+++ scripts/bwrap-common.sh
@@ -1,4 +1,5 @@
 while read -r LINE; do
 	LINE=${LINE#* }
+	[[ -x ${LINE} && -s ${LINE} ]] || continue
 	set -- --ro-bind "${LINE}" "${LINE}" "${@}"
 done < <(grep "^interpreter " /proc/sys/fs/binfmt_misc/qemu-* 2>/dev/null)
